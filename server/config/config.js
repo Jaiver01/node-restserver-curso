95eece7503213bc5c9ad93f2bd6255ccdfde4354
Jaiver Ariza <jeag-1995@hotmail.com>
2018-07-08 17:23:39 -0500

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,23 @@
-//Puerto
-process.env.PORT=process.env.PORT || 3000;
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:Jaiver01@ds129831.mlab.com:29831/cafe-db-node';
+}
+process.env.URLDB = urlDB;
