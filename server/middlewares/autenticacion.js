1e48b0615669c9dd8bcdb7c4cb91cbcfac1b9018
Jaiver Ariza <jeag-1995@hotmail.com>
2018-07-09 19:46:26 -0500

    Login normal. Seccion 10

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,45 @@
+const jwt=require('jsonwebtoken');
+// ============================
+//  Verifica token
+// ============================
+let verificaToken=(req, res, next)=>{
+	let token =req.get('token');
+
+	jwt.verify(token, process.env.SEED, (err, decoded)=>{
+		if(err){
+			return res.status(401).json({
+				ok: false,
+				err:{
+					message: 'Token no válido'
+				}
+			});
+		}
+
+		req.usuario=decoded.usuario;
+
+		next();
+	});
+};
+
+// ============================
+//  Verifica admin_role
+// ============================
+let verificaAdminRole=(req, res, next)=>{
+	let usuario=req.usuario;
+
+	if(usuario.role!=='ADMIN_ROLE'){
+		return res.status(401).json({
+			ok: false,
+			err:{
+				message: 'Usuario no autorizado'
+			}
+		});
+	}
+
+	next();
+};
+
+module.exports={
+	verificaToken,
+	verificaAdminRole
+}
